24ff3facfb69428d2b8a5d468875eb14c1dd3807
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2020-07-02 13:17:24 +1200

    Add exercise

--- src/MinutesToYearsDaysCalculator.java
+++ src/MinutesToYearsDaysCalculator.java
@@ -1,4 +1,24 @@
 public class MinutesToYearsDaysCalculator {
 
-    
+    /*
+     * Write a method printYearsAndDays with a parameter of type long named minutes
+     *
+     * The method should not return anything (void) and it needs to calculate the years and days
+     * from the minutes parameter
+     *
+     * If the parameter is less than 0, print text "Invalid Value"
+     *
+     * Otherwise, if the parameter is valid then it needs to print a message
+     * in the format "XX min = YY y and ZZ d"
+     *
+     * TIPS:
+     * - use the remainder operator
+     * 1 hour = 60 minutes
+     * 1 day = 24 hours
+     * 1 year = 365 days
+     *
+     */
+
+
+
 }
